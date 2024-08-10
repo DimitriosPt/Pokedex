7e29bd6bb681ea8f42df245346b77dce5ec94d43
ptdimitrios <ptdimitrios@gmail.com>
2024-08-09 19:45:14 -0700

    change starting state of app to ask for an entry

--- ReactApp1.client/src/Views/statblock.tsx
+++ ReactApp1.client/src/Views/statblock.tsx
@@ -15,13 +15,14 @@ function Statblock({ pokemonName }: Props) {
     const [pokemonStats, setStats] = useState<Statblock>(
         {
             name: "pikachu",
-            stats: [
-                ["hp", 0],
-                ["attack", 0],
-                ["defense", 0],
-                ["special-attack", 0],
-                ["special-defense", 0],
-                ["speed", 0]]
+            stats: {
+                hp: 0,
+                attack: 0,
+                defense: 0,
+                "special-attack": 0,
+                "special-defense": 0,
+                speed: 0
+            }
         })
 
     useEffect(() => {
