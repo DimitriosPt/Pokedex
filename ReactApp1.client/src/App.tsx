4ceb33fc0e556ccddd05dac3cac5dfafd50e2a47
ptdimitrios <ptdimitrios@gmail.com>
2024-08-10 11:39:08 -0700

    search display isn't killed between pokemon now

--- ReactApp1.client/src/App.tsx
+++ ReactApp1.client/src/App.tsx
@@ -1,23 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import FindPokemon from './Views/findPokemon';
 import Statblock from './Views/statblock';
 
 function App() {
     const [search, setSearch] = useState("");
+    const [validPokemonName, setPokemonName] = useState("");
+
+    useEffect(() =>
+    {
+        fetch(`findPokemon/${search}`).then((response) =>
+        {
+            if (response.ok) {
+                setPokemonName(search);
+            }
+            else
+            {
+                setPokemonName(validPokemonName);
+            }
+        });
+    }), [search];
 
     return (
         <div>
             <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
 
-            {search === undefined || search.length === 0 ?
+            {validPokemonName === undefined || validPokemonName.length === 0 ?
                 <p>Enter a pokemon to start</p> : 
                 <div className='parent grid-parent'>
                     <div className='child'>
-                        <FindPokemon pokemonName={search} />
+                        <FindPokemon pokemonName={validPokemonName} />
                     </div>
                     <div className='child' style={{ marginTop: '20%' }}>
-                        <Statblock pokemonName={search} />
+                        <Statblock pokemonName={validPokemonName} />
                     </div>
                 </div>
             }
